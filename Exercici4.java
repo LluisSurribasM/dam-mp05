03ed61dcb494824e10f284dfadb24832f82bca6b
LluisSurribasM <lluis.surribas@institutvidreres.cat>
2020-10-28 16:19:09 +0100

    Create Exercici4.java

---
+++ Exercici4.java
@@ -0,0 +1,9 @@
+public static void main(String[] args) {
+    int i = 5;
+    if (i %2 == 0){
+        System.out.println("Es parell");
+    } else {
+        System.out.println("Es imparell");
+    }
+    }
+}
